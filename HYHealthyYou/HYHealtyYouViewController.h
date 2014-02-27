922f26074a4db38cf7ffb6137d7064601d291125
ammy <ammy.jackmann>
2014-02-27 16:49:14 +0530

    Initial Commit

---
+++ HYHealthyYou/HYHealtyYouViewController.h
@@ -0,0 +1,13 @@
+//
+//  HYHealtyYouViewController.h
+//  HYHealthyYou
+//
+//  Created by Manish Parihar on 27/02/14.
+//  Copyright (c) 2014 MediIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HYHealtyYouViewController : UIViewController
+
+@end
---
+++ HYHealthyYou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HYHealthyYou
+//
+//  Created by Manish Parihar on 27/02/14.
+//  Copyright (c) 2014 MediIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HYHealtyYouAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HYHealtyYouAppDelegate class]));
+    }
+}
